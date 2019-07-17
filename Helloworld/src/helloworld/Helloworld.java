0ea368cab153a4ba1bd17190d496cbe2b67f56cf
lycuong99 <52919981+lycuong99@users.noreply.github.com>
2019-07-17 21:01:55 +0700

    Cuong

--- Helloworld/src/helloworld/Helloworld.java
+++ Helloworld/src/helloworld/Helloworld.java
@@ -17,6 +17,7 @@ public class Helloworld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        System.out.println("abc");
     }
     
 }
