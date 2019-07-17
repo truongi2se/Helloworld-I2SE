9b719059965680e3601a5effea6de965ebcaa744
hoanglam <hlam5370@gmail.com>
2019-07-17 22:35:14 +0700

    Lam

--- Helloworld/src/helloworld/Helloworld.java
+++ Helloworld/src/helloworld/Helloworld.java
@@ -19,7 +19,7 @@ public class Helloworld {
         System.out.println("Hello world");
         System.out.println("xyz");
         System.out.println("abc");
-        System.out.println("Truong");
+        System.out.println("Lam");
         System.out.println("Lam");
         System.out.println("Cuong");
     }
