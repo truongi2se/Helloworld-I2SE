b0335bfc1181fbc0676c443c10ea014c832ff0a6
lycuong99 <52919981+lycuong99@users.noreply.github.com>
2019-07-17 20:51:53 +0700

    Revert "Cuong"
    This reverts commit c0171808313cb92cc7ac6e14cdd962c01b0b2640, reversing
    changes made to 341a003b8fb2efb8214cd7ccb8b1209d165d9c4a.

--- Helloworld/src/helloworld/Helloworld.java
+++ Helloworld/src/helloworld/Helloworld.java
@@ -16,16 +16,7 @@ public class Helloworld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-<<<<<<< HEAD
         System.out.println("Hello world");
-=======
-        int a = 9;
-        int b =6;
-        String i2se = "HelloWorld";
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(i2se);
->>>>>>> e1feddee6ce382ba53d2ec4d6dfc83845f693737
     }
     
 }
