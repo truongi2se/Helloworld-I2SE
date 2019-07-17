ae2c15c958de832e3d1216d948369c4004b7673e
truongvh080999 <truongvh080999@users.noreply.github.com>
2019-07-17 22:53:35 +0700

    Create I2SE.java

---
+++ Helloworld/src/helloworld/I2SE.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Vvuhu
+ */
+public class I2SE {
+
+    public void i2se() {
+        System.out.println("I2SE");
+    }
+}
